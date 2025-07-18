3ff9a3d27be85b2a80f5b2307c3505b4f0b656c0
Mohammed Abed <25MohaMG@gmail.com>
2025-07-18 17:16:03 +0300

    This is a C++ map

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <map>
 
 int main()
 {
-    std::cout << "Hello World\n";
+
+    std::map<std::string, std::string> myMap {
+        {"iPhone", "Too Pricy"}, {"Samsung", " Good"},
+        {"Huawi", "Sigma"}     , {"Other", "NaN"}
+    };
+
+    std::cout << myMap["iPhone"] << "\n";
 }
